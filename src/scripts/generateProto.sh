f16b0970596a67f89c06c75eb86ce6faff57fa40
kirillbeldyaga <kirill.beldyaga@gmail.com>
2021-04-21 14:11:54 +0300

    proto moved to src/proto

--- src/scripts/generateProto.sh
+++ src/scripts/generateProto.sh
@@ -4,8 +4,8 @@ PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 # Directory to write generated code to (.js and .d.ts files)
 OUT_DIR="src/gen"
 
-protoc -I"src/types/proto" \
+protoc -I"src/proto" \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
     --js_out="import_style=commonjs,binary:${OUT_DIR}" \
     --ts_out="${OUT_DIR}" \
-    src/types/proto/auth/query.proto
+    src/proto/auth/query.proto
