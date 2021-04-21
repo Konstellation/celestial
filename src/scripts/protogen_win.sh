e76c4e01f024a986d5866bb188c5117c01df8165
LodurrWasTaken <mrr3alme@gmail.com>
2021-04-21 16:48:49 +0300

    KB-156 win script fix

--- src/scripts/protogen_win.sh
+++ src/scripts/protogen_win.sh
@@ -1,12 +1,13 @@
 # Path to this plugin
-PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
+PROTOC_GEN_TS_PATH="$PWD/node_modules/.bin/protoc-gen-ts.cmd"
+PROTOC_GEN_GRPC_PATH="$PWD/node_modules/.bin/grpc_tools_node_protoc_plugin.cmd"
 
 # Directory to write generated code to (.js and .d.ts files)
 OUT_DIR="src/types"
 
-PROTOC_GEN_GRPC_PATH="./node_modules/.bin/grpc_tools_node_protoc_plugin"
-
 protoc -I"src/proto" \
+    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
+    --plugin="protoc-gen-grpc=${PROTOC_GEN_GRPC_PATH}" \
     --js_out="import_style=commonjs,binary:${OUT_DIR}" \
     --ts_out="service=grpc-web:${OUT_DIR}" \
     --grpc_out="${OUT_DIR}" \
