3ac97c8ab30ddf947a6f441b2e9cf914bea98f4b
LodurrWasTaken <mrr3alme@gmail.com>
2021-04-21 13:44:03 +0300

    KB-156 import issue

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,22 @@
+// import { bankClient } from './modules/bank/api';
 import { authClient } from './modules/auth/api';
-console.log(authClient);
-// @ts-ignore
+
+//@ts-ignore
 authClient.Account({ address: 'darc1rzdt9wrzwv3x7vv6f7xpyaqqgf3lt6phptqtsx' }, (err, res) => {
-    // @ts-ignore
-    if (err) throw err;
-
-    console.log('Account: ');
-    // @ts-ignore
     console.log(res);
 });
-
-// @ts-ignore
-authClient.Params({}, (err, res) => {
-    // @ts-ignore
-    if (err) throw err;
-
-    console.log('params: ');
-    // @ts-ignore
-    console.log(res);
-});
-
-// @ts-ignore
-// authClient.Accounts({ pagination: { offset: 1, limit: 10, count_total: true, reverse: false } }, (err, res) => {
+// // @ts-ignore
+// bankClient.Balance(
+//     {
+//         address: 'darc1rzdt9wrzwv3x7vv6f7xpyaqqgf3lt6phptqtsx',
+//         denom: 'darc',
+//     },
 //     // @ts-ignore
-//     if (err) throw err;
-
-//     console.log('all accs: ');
-//     // @ts-ignore
-//     console.log(res);
-// });
+//     (err, res) => {
+//         console.log(res);
+//     },
+// );
 
 export class ModuleManager {
     constructor() {}
 }
-
-// import { StargateClient} from "@cosmjs/stargate";
-//
-// const hello = async () => {
-//     const client = await StargateClient.connect("206.81.29.202:26657");
-//
-//     const account = await client.getAccount('darc1rzdt9wrzwv3x7vv6f7xpyaqqgf3lt6phptqtsx');
-//     const height = await client.getHeight();
-//     console.log(height);
-//
-//     client.disconnect();
-//
-// }
-// hello();
--- src/scripts/generateProto.sh
+++ src/scripts/generateProto.sh
@@ -4,8 +4,8 @@ PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 # Directory to write generated code to (.js and .d.ts files)
 OUT_DIR="src/gen"
 
-protoc -I"C:\Program Files\protoc\include" \
+protoc -I"src/types/proto" \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
     --js_out="import_style=commonjs,binary:${OUT_DIR}" \
     --ts_out="${OUT_DIR}" \
-    src/modules/auth/proto/query.proto
+    src/types/proto/auth/query.proto
