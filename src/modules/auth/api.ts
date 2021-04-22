7c4621b9401480559dc11e6a29d3dfa7c69f1211
kirillbeldyaga <kirill.beldyaga@gmail.com>
2021-04-22 12:10:30 +0300

    KB-156 return reject

--- src/modules/auth/api.ts
+++ src/modules/auth/api.ts
@@ -6,25 +6,20 @@ import { QueryAccountRequest } from '../../types/auth/query_pb';
 
 const authClient = new QueryClient('206.81.29.202:9090', credentials.createInsecure());
 
-export const getAccount = (address: string): Promise<BaseAccount | undefined> => {
+export const getAccount = (address: string): Promise<BaseAccount> => {
     return new Promise((resolve, reject) => {
         const request = new QueryAccountRequest();
         request.setAddress(address);
 
         authClient.account(request, (err, res) => {
-            if (err) reject(err);
-
-            const accountAny = res.getAccount() as Any;
-            if (!accountAny) {
-                // todo reject error: account undefined
-                // res(err);
+            if (err) {
+                return reject(err);
             }
 
-            const acc = accountAny.unpack(BaseAccount.deserializeBinary, accountAny.getTypeName());
-            if (!acc) {
-                reject(err);
-            }
-            resolve(acc as BaseAccount);
+            const accountAny = res.getAccount();
+
+            const acc = accountAny?.unpack(BaseAccount.deserializeBinary, accountAny?.getTypeName()) as BaseAccount;
+            resolve(acc);
         });
     });
 };
