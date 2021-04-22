117135e7619d520892e93a7e2352a27b1f021343
kirillbeldyaga <kirill.beldyaga@gmail.com>
2021-04-22 11:45:01 +0300

    KB-156 get account grpc any

--- src/modules/auth/api.ts
+++ src/modules/auth/api.ts
@@ -1,11 +1,12 @@
+import { Any } from 'google-protobuf/google/protobuf/any_pb';
 import { credentials } from 'grpc';
 import { BaseAccount } from '../../types/auth/auth_pb';
 import { QueryClient } from '../../types/auth/query_grpc_pb';
-import { QueryAccountRequest, QueryAccountResponse } from '../../types/auth/query_pb';
+import { QueryAccountRequest } from '../../types/auth/query_pb';
 
 const authClient = new QueryClient('206.81.29.202:9090', credentials.createInsecure());
 
-export const getAccount = (address: string): Promise<QueryAccountResponse> => {
+export const getAccount = (address: string): Promise<BaseAccount | undefined> => {
     return new Promise((resolve, reject) => {
         const request = new QueryAccountRequest();
         request.setAddress(address);
@@ -13,8 +14,17 @@ export const getAccount = (address: string): Promise<QueryAccountResponse> => {
         authClient.account(request, (err, res) => {
             if (err) reject(err);
 
-            console.log(res);
-            resolve(res);
+            const accountAny = res.getAccount() as Any;
+            if (!accountAny) {
+                // todo reject error: account undefined
+                // res(err);
+            }
+
+            const acc = accountAny.unpack(BaseAccount.deserializeBinary, accountAny.getTypeName());
+            if (!acc) {
+                reject(err);
+            }
+            resolve(acc as BaseAccount);
         });
     });
 };
