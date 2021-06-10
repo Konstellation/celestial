8a945c6373536bbb5f8c6c0732f8658cf3050b6b
LodurrWasTaken <mrr3alme@gmail.com>
2021-06-10 20:46:06 +0300

    KB-155 protogen script

--- src/scripts/protogen.sh
+++ src/scripts/protogen.sh
@@ -1,14 +1,29 @@
-# Path to this plugin
-PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
-PROTOC_GEN_GRPC_PATH="./node_modules/.bin/grpc_tools_node_protoc_plugin"
+#!/bin/bash
 
-# Directory to write generated code to (.js and .d.ts files)
-OUT_DIR="src/types"
+COSMOS_DIR="./cosmos-sdk"
+ZIP_FILE="$COSMOS_DIR/tmp.zip"
+COSMOS_SDK_REF="0.42.0"
+COSMOS_SDK_DIR="$COSMOS_DIR/cosmos-sdk-$COSMOS_SDK_REF"
 
-protoc -I"src/proto" \
-    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
-    --plugin="protoc-gen-grpc=${PROTOC_GEN_GRPC_PATH}" \
-    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
-    --ts_out="service=grpc-web:${OUT_DIR}" \
-    --grpc_out="${OUT_DIR}" \
-    src/proto/*/*.proto src/proto/*/*/*.proto src/proto/*.proto
+mkdir -p $COSMOS_DIR
+
+wget -qO $ZIP_FILE "https://github.com/cosmos/cosmos-sdk/archive/v$COSMOS_SDK_REF.zip"
+unzip -o $ZIP_FILE -d $COSMOS_DIR
+
+THIRD_PARTY_PROTO_DIR="$COSMOS_SDK_DIR/third_party/proto"
+COSMOS_PROTO_DIR="$COSMOS_SDK_DIR/proto"
+OUT_DIR="./src/codec/"
+PROTOC_GEN_TS_PATH="$(npm bin ts-proto)/protoc-gen-ts_proto$([[ $OSTYPE == "msys" ]] && echo ".cmd")"
+
+mkdir -p $OUT_DIR
+
+protoc \
+  --plugin="protoc-gen-ts_proto=${PROTOC_GEN_TS_PATH}" \
+  --ts_proto_out=$OUT_DIR \
+  --proto_path=$COSMOS_PROTO_DIR \
+  --proto_path=$THIRD_PARTY_PROTO_DIR \
+  --ts_proto_opt="esModuleInterop=true,forceLong=long,useOptionals=true" \
+  $COSMOS_PROTO_DIR/*/*/*/*.proto \
+  $COSMOS_PROTO_DIR/*/*/*/*/*.proto
+
+rm -rf $COSMOS_DIR
